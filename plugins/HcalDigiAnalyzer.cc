45c43d50b1f2cc90d4c5571c9860d145d99f3416
Long <long.wang@cern.ch>
2021-08-12 15:34:12 +0200

    initial commit for hcal QIE11 digi analyzer

---
+++ plugins/HcalDigiAnalyzer.cc
@@ -0,0 +1,208 @@
+// -*- C++ -*-
+//
+// Package:    HcalDigiAnalyzer/HcalDigiAnalyzer
+// Class:      HcalDigiAnalyzer
+//
+/**\class HcalDigiAnalyzer HcalDigiAnalyzer.cc HcalDigiAnalyzer/HcalDigiAnalyzer/plugins/HcalDigiAnalyzer.cc
+
+ Description: [one line class summary]
+
+ Implementation:
+     [Notes on implementation]
+*/
+//
+// Original Author:  Long Wang
+//         Created:  Thu, 12 Aug 2021 12:32:06 GMT
+//
+//
+
+
+// system include files
+#include <memory>
+
+// user include files
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/one/EDAnalyzer.h"
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/Utilities/interface/InputTag.h"
+#include "FWCore/Utilities/interface/EDGetToken.h"
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "CommonTools/UtilAlgos/interface/TFileService.h"
+
+#include "DataFormats/HcalDetId/interface/HcalSubdetector.h"
+#include "DataFormats/HcalDetId/interface/HcalDetId.h"
+#include "DataFormats/HcalDetId/interface/HcalGenericDetId.h"
+#include "DataFormats/HcalDigi/interface/HcalDigiCollections.h"
+#include "DataFormats/EcalDigi/interface/EcalDigiCollections.h"
+#include "DataFormats/HcalRecHit/interface/HcalRecHitCollections.h"
+#include "DataFormats/HcalRecHit/interface/CaloRecHitAuxSetter.h"
+#include "DataFormats/HcalRecHit/test/HcalRecHitDump.cc"
+
+#include "CalibFormats/HcalObjects/interface/HcalDbService.h"
+#include "CalibFormats/HcalObjects/interface/HcalDbRecord.h"
+#include "CalibFormats/HcalObjects/interface/HcalCoderDb.h"
+
+#include "Geometry/CaloGeometry/interface/CaloGeometry.h"
+#include "Geometry/CaloGeometry/interface/CaloSubdetectorGeometry.h"
+#include "Geometry/CaloGeometry/interface/CaloCellGeometry.h"
+#include "Geometry/Records/interface/CaloGeometryRecord.h"
+
+#include "TH1.h"
+#include "TH2.h"
+#include "TH3.h"
+#include "TTree.h"
+#include "TNtuple.h"
+
+//
+// class declaration
+//
+
+// If the analyzer does not use TFileService, please remove
+// the template argument to the base class so the class inherits
+// from  edm::one::EDAnalyzer<>
+// This will improve performance in multithreaded jobs.
+
+
+class HcalDigiAnalyzer : public edm::one::EDAnalyzer<edm::one::SharedResources>  {
+   public:
+      explicit HcalDigiAnalyzer(const edm::ParameterSet&);
+      ~HcalDigiAnalyzer();
+
+      static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
+
+
+   private:
+      virtual void beginJob() override;
+      virtual void analyze(const edm::Event&, const edm::EventSetup&) override;
+      virtual void endJob() override;
+
+      // ----------member data ---------------------------
+      edm::EDGetTokenT<QIE11DigiCollection> qie11digisToken_;
+
+      TH3I* hist3D;
+
+};
+
+//
+// constants, enums and typedefs
+//
+
+//
+// static data member definitions
+//
+
+//
+// constructors and destructor
+//
+HcalDigiAnalyzer::HcalDigiAnalyzer(const edm::ParameterSet& iConfig)
+ :
+  qie11digisToken_(consumes<QIE11DigiCollection>(iConfig.getUntrackedParameter<edm::InputTag>("tagQIE11", edm::InputTag("hcalDigis"))))
+{
+   //now do what ever initialization is needed
+   usesResource("TFileService");
+   edm::Service<TFileService> fs;
+
+   hist3D = fs->make<TH3I>("hist3D", "hist3D", 1000, 0, 1000, 64, -32, 32, 72, 0, 72);
+
+}
+
+
+HcalDigiAnalyzer::~HcalDigiAnalyzer()
+{
+
+   // do anything here that needs to be done at desctruction time
+   // (e.g. close files, deallocate resources etc.)
+
+}
+
+
+//
+// member functions
+//
+
+// ------------ method called for each event  ------------
+void
+HcalDigiAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
+{
+   using namespace edm;
+
+#ifdef THIS_IS_AN_EVENT_EXAMPLE
+   Handle<ExampleData> pIn;
+   iEvent.getByLabel("example",pIn);
+#endif
+
+#ifdef THIS_IS_AN_EVENTSETUP_EXAMPLE
+   ESHandle<SetupData> pSetup;
+   iSetup.get<SetupRecord>().get(pSetup);
+#endif
+
+  long runid   = iEvent.id().run();
+  long eventid = iEvent.id().event();
+  long lumiid  = iEvent.id().luminosityBlock();
+
+  edm::ESHandle<HcalDbService> conditions;
+  iSetup.get<HcalDbRecord>().get(conditions);
+
+  edm::Handle<QIE11DigiCollection> qie11Digis;
+  bool gotQIE11Digis = iEvent.getByToken(qie11digisToken_, qie11Digis);
+  if (!gotQIE11Digis)
+    std::cout << "Could not find HCAL QIE11Digis with tag: qie11Digis" << std::endl;
+
+  for (QIE11DigiCollection::const_iterator it = qie11Digis->begin(); it != qie11Digis->end(); ++it) {
+    const QIE11DataFrame digi = static_cast<const QIE11DataFrame>(*it);
+
+    HcalDetId const& did = digi.detid();
+    //if(did.subdet() != HcalEndcap) continue;
+
+    const HcalQIECoder* channelCoder = conditions -> getHcalCoder(did);
+    const HcalQIEShape* shape = conditions -> getHcalShape(channelCoder);
+    HcalCoderDb coder(*channelCoder,*shape);
+    CaloSamples cs; coder.adc2fC(digi,cs);
+
+    float charge_ = 0.;
+    for(int i=0; i<digi.samples(); i++){
+      charge_ += cs[i];
+    }
+
+    if(charge_>500)
+      hist3D->Fill(lumiid, did.ieta(), did.iphi());
+  }
+
+
+}
+
+
+// ------------ method called once each job just before starting event loop  ------------
+void
+HcalDigiAnalyzer::beginJob()
+{
+}
+
+// ------------ method called once each job just after ending the event loop  ------------
+void
+HcalDigiAnalyzer::endJob()
+{
+}
+
+// ------------ method fills 'descriptions' with the allowed parameters for the module  ------------
+void
+HcalDigiAnalyzer::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
+  //The following says we do not know what parameters are allowed so do no validation
+  // Please change this to state exactly what you do use, even if it is no parameters
+  edm::ParameterSetDescription desc;
+  desc.setUnknown();
+  descriptions.addDefault(desc);
+
+  //Specify that only 'tracks' is allowed
+  //To use, remove the default given above and uncomment below
+  //ParameterSetDescription desc;
+  //desc.addUntracked<edm::InputTag>("tracks","ctfWithMaterialTracks");
+  //descriptions.addDefault(desc);
+}
+
+//define this as a plug-in
+DEFINE_FWK_MODULE(HcalDigiAnalyzer);
---
+++ python/ConfFile_cfg.py
@@ -0,0 +1,60 @@
+import FWCore.ParameterSet.Config as cms
+import FWCore.ParameterSet.VarParsing as VarParsing
+
+from Configuration.StandardSequences.Eras import eras
+process = cms.Process('HcalDigiAnalyzer',eras.Run2_2018)
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+process.load('Configuration.StandardSequences.Services_cff')
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32(500)
+
+process.load('Configuration.EventContent.EventContent_cff')
+process.load("Configuration.StandardSequences.GeometryDB_cff")
+process.load("Configuration.StandardSequences.MagneticField_cff")
+process.load("Configuration.StandardSequences.RawToDigi_Data_cff")
+process.load("Configuration.StandardSequences.Reconstruction_cff")
+process.load("RecoLocalCalo.Configuration.hcalLocalReco_cff")
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+#process.load("RecoLuminosity.LumiProducer.bunchSpacingProducer_cfi")
+
+#process.load("RecoLocalTracker.SiPixelRecHits.SiPixelRecHits_cfi")
+
+process.source = cms.Source("PoolSource",
+        fileNames = cms.untracked.vstring(
+        '/store/data/Run2018D/ZeroBias/RAW/v1/000/325/169/00000/A9C40B8C-593C-A940-9DFC-D4B841D70C9C.root',
+	'/store/data/Run2018D/ZeroBias/RAW/v1/000/325/169/00000/3E1AA73B-205F-A84C-8E55-A4BECA86399F.root',
+	'/store/data/Run2018D/ZeroBias/RAW/v1/000/325/169/00000/758FA122-5717-C541-AB1E-1432CA535C9C.root',
+	'/store/data/Run2018D/ZeroBias/RAW/v1/000/325/169/00000/05B70CF9-46C3-3446-B757-B6BEF1C49C61.root',
+	'/store/data/Run2018D/ZeroBias/RAW/v1/000/325/169/00000/08326CEE-FC91-D741-888C-E37FFF32A79C.root',
+	'/store/data/Run2018D/ZeroBias/RAW/v1/000/325/169/00000/FF7B77A6-996D-1B40-AE72-1AEAC4164E40.root'
+        )
+)
+
+process.options = cms.untracked.PSet(
+#	SkipEvent = cms.untracked.vstring('ProductNotFound')
+)
+
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+from Configuration.AlCa.autoCond import autoCond
+process.GlobalTag.globaltag = '101X_dataRun2_v8'
+
+
+process.HcalDigiAnalyzer = cms.EDAnalyzer('HcalDigiAnalyzer',
+	tagQIE11 = cms.untracked.InputTag("hcalDigis"),
+)
+
+process.TFileService = cms.Service("TFileService",
+      fileName = cms.string("QIE11DigiOccupancy.root"),
+      closeFileFast = cms.untracked.bool(True)
+)
+
+
+process.p = cms.Path(
+	process.bunchSpacingProducer*
+        process.hcalDigis*
+#	process.hcalLocalRecoSequence*
+	process.HcalDigiAnalyzer
+)
+
+#process.outpath = cms.EndPath(process.out)
